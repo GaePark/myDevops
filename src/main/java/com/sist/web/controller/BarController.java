af6a6b136a1c7672e008108ef907ed5cebad0cda
GaePark <qkrwnsgus01@gmail.com>
2025-06-13 16:35:40 +0900

    캐시테스트

---
+++ src/main/java/com/sist/web/controller/BarController.java
@@ -0,0 +1,12 @@
+package com.sist.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.sist.web.entity.BarEntity;
+
+@Controller
+public class BarController {
+	
+}
